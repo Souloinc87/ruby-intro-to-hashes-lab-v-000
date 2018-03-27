19e6a7b706d9beea660caf6cf972a9bd9334fa8e
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-27 06:41:22 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ end
 def monopoly
 	monopoly = {
 		:railroads => {}
-		
+
 	}
 
 
@@ -21,9 +21,10 @@ def monopoly_with_second_tier
 
 	monopoly = {
 		:railroads => {
-			:pieces => 4
+			:pieces => 4,
+			:names => {}
 		}
-		
+
 	}
 
 
